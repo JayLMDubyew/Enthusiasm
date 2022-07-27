bba008658b34de3b4fc0a1c8ca52072827aae0f0
JayLMDubyew <jesswcomm@gmail.com>
2022-07-27 15:05:32 -0400

    ciphers 1.0 published

---
+++ Ciphers/cipher_implementation.py
@@ -0,0 +1,38 @@
+## JayLMDubyew (JLMW) - 2022
+import random
+import pprint
+pp = pprint.PrettyPrinter(width=41, compact=True)
+
+# useful for cesar, I guess
+def rot(rotation_factor, message):
+    ciphertext = ""
+    for c in message:
+        # rotate by rotation_factor
+        if c.isupper():
+            ciphertext += chr((ord(c) + rotation_factor - 65) % 26 + 65)
+        else:
+            ciphertext += chr((ord(c) + rotation_factor - 97) % 26 + 97)
+    return ciphertext
+
+#bruteforce rotation
+def brute_rot(ciphertext):
+    for i in range(26):
+        print(f"Attempt {i+1} (Shift by {26-i}): {rot(i,ciphertext)}")
+
+
+# monoalphabetic cipher
+def monoalpha(message):
+    alphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
+    cipher = list(alphabet)
+    random.shuffle(cipher)
+    dictionary = dict(zip(list(alphabet) , cipher))
+    print("Your Cipher:")
+    pp.pprint(dictionary)
+
+    ciphertext=""
+    for c in message:
+        ciphertext += dictionary.get(c, c)
+    print(ciphertext)
+
+print(rot(4,"Hello"))
+brute_rot("Lipps")
